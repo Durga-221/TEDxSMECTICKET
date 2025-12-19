a2bb511d4ae39c67f8e5ffa083e626840cafe55d
HEVEN-STORES <heven.storess@gmail.com>
2025-12-19 20:21:26 +0530

    backend

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,5 +1,29 @@
-import express from "express";
-import cors from "cors";
+// import express from "express";
+// import cors from "cors";
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.get("/api/health", (req, res) => {
+//   res.json({
+//     success: true,
+//     message: "Backend deployed successfully 🚀"
+//   });
+// });
+
+// // Example API
+// app.post("/api/test", (req, res) => {
+//   res.json({ received: req.body });
+// });
+
+// export default app;
+
+
+
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
@@ -18,4 +42,4 @@ app.post("/api/test", (req, res) => {
   res.json({ received: req.body });
 });
 
-export default app;
+module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -39,6 +39,6 @@ app.use(express.json());
 app.use("/api/qr", require("./routes/validateQR"));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`Server running on port ${PORT}`)
-);
+// app.listen(PORT, () =>
+//   console.log(`Server running on port ${PORT}`)
+// );
